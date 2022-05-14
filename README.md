40e6785b238a64759127061a88ccc25d7cb944d8
Wilson Francisco <wilsonfranciscogamboa123@gmail.com>
2022-05-14 16:22:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# javacript
+ Estudo de JAVACRIPT
---
+++ exercicio/teste07.js
@@ -0,0 +1,8 @@
+function numeroVezesLetra(frase){
+    var contadorLetra = [];
+    for (var indiceFrase = 0; indiceFrase < frase.length; indiceFrase++){
+        contadorLetra.push(frase[indiceFrase])
+    }
+return contadorLetra;
+}
+numeroVezesLetra('wilson')
